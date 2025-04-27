778a759839c5615f381bc0ce3fa6bbc74d721f53
roopemyller <roope.myller@gmail.com>
2025-04-27 14:42:08 +0300

    client init

---
+++ client/README.md
@@ -0,0 +1,24 @@
+
+### Create .venv under the client
+
+`python -m venv .venv`
+
+### Activate venv
+
+`source .venv/bin/activate`(Linux/MacOS)
+
+or
+
+`.venv\Scripts\activate`(Win)
+
+### Install requirements
+
+`pip install -r requirements.txt`
+
+### Install CLI client locally
+
+`pip install -e .`
+
+### Help with cloudcli
+
+`cloudcli --help`
---
+++ client/auth.py
@@ -0,0 +1,43 @@
+import typer
+import requests
+from utils import save_token
+
+app = typer.Typer()
+SERVER = "http://localhost:5000" # Replace with REAL server URL
+
+NOSERVER = True # client dev phase
+
+# Register user
+@app.command()
+def register(username: str, password: str):
+
+    if NOSERVER:
+        typer.echo(f"Registering user {username}.")
+        return
+
+    payload = {"username": username, "password": password}
+    response = requests.post(f"{SERVER}/auth/register", json=payload)
+    if response.ok:
+        typer.echo("User registered successfully.")
+    else:
+        typer.echo(f"Failed to register user: {response.text}")
+
+# Login user and save token
+@app.command()
+def login(username: str, password: str):
+
+    if NOSERVER:
+        typer.echo(f"Login user {username}.")
+        return
+
+    payload = {"username": username, "password": password}
+    response = requests.post(f"{SERVER}/auth/login", json=payload)
+    if response.ok:
+        token = response.json().get("token")
+        if not token:
+            typer.echo("No token received.")
+            return
+        save_token(token)
+        typer.echo(f"User {username} logged in successfully.")
+    else:
+        typer.echo(f"Failed to log in: {response.text}")
